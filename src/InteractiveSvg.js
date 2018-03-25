6df6347086703aa0cacb668acd9c7015d3fc1bca
Alex Ilatovsky <ilatovsky@air.local>
2018-03-25 14:24:55 +0300

    InteractiveSvg refactor

--- src/InteractiveSvg.js
+++ src/InteractiveSvg.js
@@ -14,13 +14,13 @@ const hammerOptions = {
       enable: true
     },
     pan:{
-
+      threshold:10,
+      pointers:1
     }
   }
 };
 
 const getTransform = ({pan,pinch,x,y,currentX,currentY,scale,currentScale,center}) => {
-
   let left,top,divScale;
   if (pinch) {
     left = (x - center.x * currentScale) * 100;
@@ -36,7 +36,6 @@ const getTransform = ({pan,pinch,x,y,currentX,currentY,scale,currentScale,center
       top = (y - center.y * scale) * 100;
     };
   }
-
   return {top,left,divScale}
 }
 
@@ -46,12 +45,6 @@ const getRatio = (planDimension, componentDimension) =>{
   let divisionX = 1;
   let divisionY = pAsp / cAsp;
   return {pAsp,cAsp,divisionX,divisionY};
-}
-
-const fitScaleCalculate = (planDimension, componentDimension) => {
-  let fitWidth = componentDimension.width / planDimension.width;
-  let fitHeight = componentDimension.height / planDimension.height;
-  return {fitHeight,fitWidth};
 }
 
 // const rangecheck = (x,y,center,scale,fitScale) => {
@@ -68,16 +61,10 @@ class InteractiveSVG extends Component {
     super(props);
     let XMLP = new XMLParser();
     let levels = this.props.levels.map(level => XMLP.parseFromString(level.raw));
-    let [top, left, width, height] = levels[0].attributes.viewBox.split(' ');
+    let [, , width, height] = levels[0].attributes.viewBox.split(' ');
     let planDimension = {
       width,
       height
-    };
-
-    let fitScale = fitScaleCalculate(planDimension, this.props.size);
-    let center = {
-      y: height * fitScale.fitWidth / 2 / this.props.size.height,
-      x: width * fitScale.fitWidth / 2 / this.props.size.width
     };
 
     let ratio = getRatio(planDimension, this.props.size);
@@ -85,8 +72,7 @@ class InteractiveSVG extends Component {
     this.state = {
       planDimension: planDimension,
       ratio:ratio,
-      center: center,
-      fitScale: fitScale,
+      center: {x:ratio.divisionX/2,y:ratio.divisionY/2},
       x: 0.5,
       y: 0.5,
       currentX: 0.5,
@@ -106,56 +92,64 @@ class InteractiveSVG extends Component {
     this.handlePinchEnd = this.handlePinchEnd.bind(this);
   }
 
+
+
   componentWillReceiveProps(nextProps) {
     console.log({newProps:nextProps});
     let ratio = getRatio(this.state.planDimension, nextProps.size);
     this.setState({
-      fitScale: fitScaleCalculate(this.state.planDimension, nextProps.size),
       ratio: ratio,
       center: {x:ratio.divisionX/2,y:ratio.divisionY/2}
     });
+  }
+
+  setTransform({x=this.state.x,y=this.state.y,scale=this.state.scale,current=false}){
+    if (!current){
+        this.setState({x:x,y:y,scale:scale});
+    } else {
+        this.setState({currentX:x,currentY:y,currentScale:scale});
+    }
   }
 
   // ZOOM_EVENTS
   handleWheel(ev) {
     ev.preventDefault();
     let newScale = this.state.scale + 0.005 * ev.deltaY;
-    if (newScale > 1) {
-      this.setState({scale: newScale})
-    } else {
-      this.setState({scale: 1})
-    }
+    this.setTransform({scale:(newScale > 1)?newScale:1});
   }
+
   handlePinchStart(ev) {
     this.setState({pinch: true});
   }
+
   handlePinchEnd(ev) {
     let {currentScale} = this.state;
     this.setState({pinch: false, scale: currentScale});
   }
+
   handlePinch(ev) {
     ev.preventDefault();
     let {scale} = this.state;
-    this.setState({
-      currentScale: scale * ev.scale
-    });
+    this.setTransform({scale:scale * ev.scale,current:true});
+    // this.setState({
+    //   currentScale:
+    // });
   }
 
   // PAN_EVENTS
   handlePan(ev) {
     ev.preventDefault();
-    let {x, y, scale,center,fitScale} = this.state;
+    let {x,y} = this.state;
     let newx = x + ev.deltaX / this.props.size.width;
-    let newy = y + ev.deltaY / this.props.size.height
-    this.setState({
-      currentX: newx,
-      currentY: newy
-    })
+    let newy = y + ev.deltaY / this.props.size.height;
+    this.setTransform({x:newx,y:newy,current:true});
   }
+
   handlePanStart(ev) {
     ev.preventDefault();
     this.setState({pan: true});
   }
+
   handlePanEnd(ev) {
     ev.preventDefault();
     let {currentX, currentY} = this.state;
@@ -170,7 +164,7 @@ class InteractiveSVG extends Component {
       <div style={{
           height: '100vh',
           width: '100vw',
-          background: '#333'
+          // background: '#333'
         }}>
         <div className='svgWrap' style={{
             position: 'relative',
